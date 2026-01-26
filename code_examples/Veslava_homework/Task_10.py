dd709eb04894af3c22dfacfa26427be30643a194
OnionDatabase <6scientiapotentiaest@gmail.com>
2026-01-26 19:30:13 +0100

    Task 6-19 finished with mistakes

--- code_examples/Veslava_homework/Task_10.py
+++ code_examples/Veslava_homework/Task_10.py
@@ -1,4 +1,4 @@
 # Homework №3
 ## Тask 10: Flexible Functions
 def calculate_total(base_price, tax=0.05, *args, **kwargs):
-    print(calculate_total )
+    print(sum(base_price, base_price*tax, *args))
---
+++ code_examples/Veslava_homework/Task_11.py
@@ -0,0 +1,2 @@
+# Homework №3
+## Task 11: The Grade Analytics System
