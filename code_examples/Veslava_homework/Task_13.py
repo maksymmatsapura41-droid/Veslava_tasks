c6543aa2101f967c1ca02236e17fde657ee206c8
OnionDatabase <6scientiapotentiaest@gmail.com>
2026-01-27 18:30:53 +0100

    changes

--- code_examples/Veslava_homework/Task_13.py
+++ code_examples/Veslava_homework/Task_13.py
@@ -2,8 +2,11 @@
 ## Task 13: Advanced Password Validator
 
 #Topics: Conditions, String Methods, Ternary Operators.
-
+password_new = "2026Ten"
 def passwordcheck(stringpass):
-    if item >= len(11) in stringpass:
-        return "Valid Passowrd"
-    else ("High risk" if item >= len(6) else "Evaluation Required")
+    if len(stringpass) >= 10 and any(x.isdigit() for x in stringpass) and any(x.isupper() for x in stringpass):
+        return "Valid Password"
+    else:
+        return "High risk" if len(stringpass) < 6 else "Evaluation Required"
+
+print(passwordcheck(password_new))
--- code_examples/Veslava_homework/Task_14.py
+++ code_examples/Veslava_homework/Task_14.py
@@ -4,5 +4,13 @@
 items = [{"name": "Laptop", "price": 1200}, {"name": "Mouse", "price": 25}, {"name": "Monitor", "price": 300}]
 
 def updated(insert_item):
-    [(x*0.75 if x > 500 else x*0.95) for x in insert_item]
-    print(insert_item)
+    #[(x*0.75 if x > 500 else x*0.95) for x in insert_item]
+    for item in insert_item:
+        discount_rate = 0.85 if item["price"] > 500 else 0.95
+        item["price"] = item["price"]*discount_rate
+        #item["Mark"] = item["price"]*discount_rate #to modify key, just add new and delete old:
+        #item.pop("price") #Remove the old key
+        #del item["price"] #Remove old key
+    return insert_item
+
+print(updated(items))
